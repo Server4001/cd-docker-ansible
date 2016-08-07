44ef65ed2477e75b24ead549c851ef0c5594675a
Brice Bentler <server4001@gmail.com>
2016-08-07 05:16:45 -0700

    Changes python and mysql versions so that python mysql libs can be installed in provisioning script

--- provision.sh
+++ provision.sh
@@ -1,35 +1,18 @@
 #!/usr/bin/env bash
 
-# Install Yum utils.
-sudo yum install -y yum-utils
-
-# Python 3.4 install.
-if [ ! -f /usr/local/bin/python3.4 ]; then
-    sudo yum groupinstall -y 'development tools'
-    sudo yum install -y zlib-dev openssl-devel sqlite-devel bzip2-devel
-    cd /usr/src
-    sudo wget https://www.python.org/ftp/python/3.4.4/Python-3.4.4.tgz
-    sudo tar xzf Python-3.4.4.tgz
-    sudo rm Python-3.4.4.tgz
-    cd Python-3.4.4/
-    ./configure
-    make
-    sudo make altinstall
-    sudo ln -s /usr/local/bin/python3.4 /usr/bin/python3.4
-    sudo ln -s /usr/local/bin/pip3.4 /usr/bin/pip3.4
-    sudo ln -s /usr/local/bin/easy_install-3.4 /usr/bin/easy_install-3.4
-    cd /usr/src
-    sudo rm -rf Python-3.4.4/
-    sudo pip3.4 install --upgrade pip
-    cd $HOME
-fi
-
 # Bashrc.
 sudo cp /vagrant/config/bash/root.bashrc /root/.bashrc
 cp /vagrant/config/bash/vagrant.bashrc $HOME/.bashrc
 
 # Install EPEL repo.
 sudo yum install -y epel-release
+
+# Install pip.
+sudo yum install -y python-pip
+sudo pip install --upgrade pip
+
+# Install Python packages.
+sudo pip install virtualenv boto boto3 awscli django djangorestframework django-cors-headers django-nose pinocchio coverage
 
 # Install vim, tree, etc.
 sudo yum install -y vim tree git man man-pages
@@ -49,9 +32,6 @@ sudo cp /vagrant/config/ntp/ntp.conf /etc/ntp.conf
 sudo systemctl start ntpd
 sudo systemctl enable ntpd
 
-# Install Python packages.
-sudo pip3.4 install virtualenv boto boto3 awscli django djangorestframework django-cors-headers django-nose pinocchio coverage
-
 # Install Node.js.
 wget https://nodejs.org/dist/v4.4.7/node-v4.4.7-linux-x64.tar.gz --no-check-certificate
 sudo tar --strip-components 1 -xzvf node-v4.4.7-linux-x64.tar.gz -C /usr/local
@@ -64,64 +44,28 @@ sudo ln -s /usr/local/bin/node /usr/bin/node
 # Install global NPM packages.
 sudo npm install -g mocha
 
-# Create variables for MySQL 5.7 install.
-MYSQL_LOG_FOLDER="/var/log/mysql"
-MYSQL_LIB_FOLDER="/var/lib/mysql"
-MYSQL_ROOT_USER_PASSWORD="password"
-MYSQL_CONFIG_FILE="/etc/my.cnf"
-ROOT_USER_MYSQL_CONFIG="/root/.my.cnf"
-VAGRANT_USER_MYSQL_CONFIG="$HOME/.my.cnf"
+# Install MySQL.
+wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+sudo yum localinstall -y mysql-community-release-el7-5.noarch.rpm
+sudo yum install -y mysql-community-server
+rm mysql-community-release-el7-5.noarch.rpm
 
-# Install MySQL 5.7.
-if [ ! -f /usr/bin/mysql ]; then
-    wget http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
-    sudo yum localinstall -y mysql-community-release-el7-5.noarch.rpm
-    rm $HOME/mysql-community-release-el7-5.noarch.rpm
-    sudo yum-config-manager --disable mysql56-community
-    sudo yum-config-manager --enable mysql57-community-dmr
-    sudo yum install -y mysql-community-server-5.7.14
-fi
-
-# Set up log folder for MySQL.
-sudo mkdir ${MYSQL_LOG_FOLDER}
-sudo chmod 0755 ${MYSQL_LOG_FOLDER}
-sudo chown mysql:mysql ${MYSQL_LOG_FOLDER}
-
-# Initialize MySQL insecurely, so that we can lock it down with a known password.
-if [ ! -f ${MYSQL_LIB_FOLDER}/mysql ]; then
-    cd ${MYSQL_LIB_FOLDER}
-    sudo mysqld --initialize-insecure
-    cd $HOME
-fi
-
-# Configure MySQL.
-sudo chown -R mysql:mysql ${MYSQL_LIB_FOLDER}
-sudo cp /vagrant/config/mysql/my.cnf ${MYSQL_CONFIG_FILE}
-sudo chmod 0644 ${MYSQL_CONFIG_FILE}
-sudo chown mysql:mysql ${MYSQL_CONFIG_FILE}
-
-# Start the MySQL service.
+# Start MySQL.
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
 
-# Create new password for root MySQL user.
-if [ ! -f ${MYSQL_LIB_FOLDER}/.mysql-root-password-has-been-set-by-provisioner ]; then
-    sudo mysqladmin -u root password ${MYSQL_ROOT_USER_PASSWORD}
-    sudo touch ${MYSQL_LIB_FOLDER}/.mysql-root-password-has-been-set-by-provisioner
-fi
-
-# Configure MySQL for root and vagrant users.
-sudo cp /vagrant/config/mysql/.my.cnf ${ROOT_USER_MYSQL_CONFIG}
-sudo chmod 0600 ${ROOT_USER_MYSQL_CONFIG}
-sudo chown root:root ${ROOT_USER_MYSQL_CONFIG}
-cp /vagrant/config/mysql/.my.cnf ${VAGRANT_USER_MYSQL_CONFIG}
-chmod 0600 ${VAGRANT_USER_MYSQL_CONFIG}
-chown vagrant:vagrant ${VAGRANT_USER_MYSQL_CONFIG}
-
 # Secure the MySQL install.
-mysql -u root -p"$MYSQL_ROOT_USER_PASSWORD" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-mysql -u root -p"$MYSQL_ROOT_USER_PASSWORD" -e "DELETE FROM mysql.user WHERE User=''"
-mysql -u root -p"$MYSQL_ROOT_USER_PASSWORD" -e "FLUSH PRIVILEGES"
+export DATABASE_PASS=password
+mysqladmin -u root password "$DATABASE_PASS"
+mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
+mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
+mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+
+# Install the MySQL libs for Python.
+sudo yum groupinstall -y 'development tools'
+sudo yum install -y mysql-community-devel python-devel
+sudo pip install mysql-python
 
 # Install nginx.
 sudo yum install -y nginx
