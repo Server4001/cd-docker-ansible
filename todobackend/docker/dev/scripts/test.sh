765dc4c67a587969e368bdc040c354e891204d12
Brice Bentler <server4001@gmail.com>
2016-08-10 08:36:05 -0700

    Adds initial docker image for development

---
+++ todobackend/docker/dev/scripts/test.sh
@@ -0,0 +1,9 @@
+#!/bin/ash
+# Activate virtual environment.
+. /appenv/bin/activate
+
+# Install application test requirements.
+pip install -r requirements_test.txt
+
+# Run test.sh arguments.
+exec $@
