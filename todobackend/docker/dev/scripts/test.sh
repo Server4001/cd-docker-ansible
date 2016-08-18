69618f878f3ea65c3d973a2a93018794d4e2a5cc
Brice Bentler <server4001@gmail.com>
2016-08-17 23:05:46 -0700

    Adds the builder container for building the application using python wheels

--- todobackend/docker/dev/scripts/test.sh
+++ todobackend/docker/dev/scripts/test.sh
@@ -2,8 +2,11 @@
 # Activate virtual environment.
 . /appenv/bin/activate
 
+# Download requirements to build cache.
+pip download -d /build -r requirements_test.txt --no-input
+
 # Install application test requirements.
-pip install -r requirements_test.txt
+pip install --no-index -f /build -r requirements_test.txt
 
 # Wait for MySQL container to become available
 dockerize -wait tcp://$MYSQL_HOST:3306
